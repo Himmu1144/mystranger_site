97880d8d15c897368d735bfb4a21ae3462a1ef8a
TheSerpentCoder <himmu1144@gmail.com>
2023-11-15 01:06:49 +0530

    webrtc is working fine as of now - 15/11/2023 - 1PM

--- mystranger_app/consumers.py
+++ mystranger_app/consumers.py
@@ -80,11 +80,13 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
                 await self.channel_layer.group_send(content['group'],{
                 'type':'offer.message',
                 'offer':content['offer'],
+                'icecandidates':content['icecandidates'],
                 'groupName' : content['group'],
             })
             elif command == 'answer':
                 await self.channel_layer.group_send(content['group'],{
                 'type':'answer.message',
+                'icecandidates':content['icecandidates'],
                 'answer':content['answer']
             })
             elif(content['command'] == 'candidate'):
@@ -543,12 +545,14 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         await self.send_json({
             'command':'offer',
             'offer':event['offer'],
+            'icecandidates' : event['icecandidates'],
             'groupName' : event['groupName'],
         })
 
     async def answer_message(self,event):
         await self.send_json({
             'command':'answer',
+            'icecandidates' : event['icecandidates'],
             'answer':event['answer']
         })
 
